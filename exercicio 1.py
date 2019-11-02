ec406ce643404e790418c8e86673b3100d3acda5
Fabio@1987 <fabiobr47@gmail.com>
2019-11-01 22:31:12 -0300

    enviei um arquivo python

---
+++ exercicio 1.py
@@ -0,0 +1,15 @@
+"""
+espaço para comentário, não fica a amostra no programa
+"""
+a = 10#comentário simples sobre a linha
+b = 20
+aux = 10
+a = b
+b = aux
+print ("variável a é =",a, " depois da troca")
+print ("variável b é =",b, " depois da troca")
+aux = 20
+a = b
+b = aux
+print ("variável a é =" ,a, "trocando denovo")
+print ("variável b é =", b, "trocando denovo")
